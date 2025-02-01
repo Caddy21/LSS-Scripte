f0c29a3a69c55d22ec37b36e020aa830fba80e37
Caddy21 <163453041+Caddy21@users.noreply.github.com>
2025-02-01 19:58:40 +0100

    [LSS] 9 - Einsatzkategorienfilter.user.js

--- [LSS] 9 - Einsatzkategorienfilter.user.js
+++ [LSS] 9 - Einsatzkategorienfilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         [LSS] 9 - Einsatzkategorienfilter
+// @name         [LSS] Einsatzkategorienfilter
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Filtert die Einsatzliste nach Kategorien
 // @author       Caddy21
 // @match        https://www.leitstellenspiel.de/
