dc12becb8170fe129e71ea5a36cf520d7c729ac8
Caddy21 <163453041+Caddy21@users.noreply.github.com>
2025-02-03 15:47:25 +0100

    [LSS] 10 - Auszeichnungsverwalter.user.js

--- [LSS] 10 - Auszeichnungsverwalter.user.js
+++ [LSS] 10 - Auszeichnungsverwalter.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         [LSS] 10 - Auszeichnungsverwalter
+// @name         [LSS] Auszeichnungssortierer
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Fügt Tabs bei den Auszeichnungen hinzu zur besseren Übersicht
