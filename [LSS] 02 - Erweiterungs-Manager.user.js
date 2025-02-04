30e25b4816b095ccd64f9f622f3104adbc3caf48
Caddy21 <163453041+Caddy21@users.noreply.github.com>
2025-02-04 18:42:47 +0100

    [LSS] 02 - Erweiterungs-Manager.user.js

--- [LSS] 02 - Erweiterungs-Manager.user.js
+++ [LSS] 02 - Erweiterungs-Manager.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         [LSS]  Erweiterungs-Manager
+// @name         [LSS] Erweiterungs-Manager
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Listet Wachen auf, bei denen bestimmte Erweiterungen fehlen und ermöglicht das Hinzufügen dieser Erweiterungen.
