583c3598c5eef13032dead1306125ea458824419
Caddy21 <163453041+Caddy21@users.noreply.github.com>
2025-03-09 08:20:20 +0100

    Update [LSS] 16 - Zielortfilter.user.js

--- [LSS] 16 - Zielortfilter.user.js
+++ [LSS] 16 - Zielortfilter.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         [LSS] Entfernungs- und Abgabenfilter
+// @name         [LSS] Zielortfilter
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Blendet Krankenhäuser, Zellen und mögliche Übergabeorte nach Kriterien aus.
