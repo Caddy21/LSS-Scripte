234a75cfb8cb8ca6b4983ad8efa41bde3ddd6ae1
Caddy21 <163453041+Caddy21@users.noreply.github.com>
2025-03-19 21:10:45 +0100

    Update [LSS] 17 - Einsätze anzeigen.user.js

--- [LSS] 17 - Einsätze anzeigen.user.js
+++ [LSS] 17 - Einsätze anzeigen.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         [LSS] Einsätze anzeigen
+// @name         [LSS] 17 - Einsätze anzeigen
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Zeigt die Einsätze an wo noch Wachen fehlen
-// @match        https://www.leitstellenspiel.de/*
+// @version      1.1
+// @description  Blendet Einsätze basierend auf individuellen Kategorien aus unter "Mögliche Einsätze"
+// @match        https://www.leitstellenspiel.de/einsaetze
 // @icon         https://github.com/Caddy21/-docs-assets-css/raw/main/yoshi_icon__by_josecapes_dgqbro3-fullview.png
 // @grant        none
 // ==/UserScript==
@@ -11,14 +11,102 @@
 (function() {
     'use strict';
 
-    function hideSuccessMissions() {
-        document.querySelectorAll('.mission_type_index_searchable.success').forEach(el => {
-            el.style.display = 'none';
+    const hideSuccess = true;  // Erfolgreiche Einsätze ausblenden
+    // Konfigurierbare Optionen (true = ausblenden, false = anzeigen)
+    const filterOptions = {
+        feuerwachen: false,
+        rettungswachen: false,
+        wasserrettungswachen: false,
+        polizeiwachen: false,
+        thwOrtsverbaende: false,
+        rettungshundestaffeln: false,
+        dienstgruppenleitung: false,
+        thwNotversorgung: false,
+        thwRaeumen: false,
+        thwZugtrupps: false,
+        thwSB: false,
+        drohnenErweiterungen: false,
+        betreuungsVerpflegung: false,
+        bereitschaftspolizei: false,
+        gefangenenkraftwagen: false,
+        hundertschaft: false,
+        verpflegungsdienst: false,
+        lautsprecherkraftwagen: false,
+        motorradstaffel: false,
+        polizeihubschrauber: false,
+        wasserwerfer: false,
+        mekWache: false,
+        sekWache: false,
+        nea50: false,
+        nea200: false,
+        bergrettungswache: false,
+        seenotrettungswache: false,
+        seenothubschrauber: false,
+        reiterstaffel: false,
+        bahnrettung: false,
+        lüfter: false,
+        windenrettung: false
+    };
+
+    // Schlagwörter und zugehörige Optionen
+    const keywords = {
+        "Feuerwache": filterOptions.feuerwachen,
+        "Rettungswache": filterOptions.rettungswachen,
+        "Wasserrettungswache": filterOptions.wasserrettungswachen,
+        "Polizeiwache": filterOptions.polizeiwachen,
+        "THW-Ortsverband": filterOptions.thwOrtsverbaende,
+        "Rettungshundestaffel": filterOptions.rettungshundestaffeln,
+        "Dienstgruppenleitung": filterOptions.dienstgruppenleitung,
+        "THW: Fachgruppe Notversorgung": filterOptions.thwNotversorgung,
+        "THW: Fachgruppe Räumen": filterOptions.thwRaeumen,
+        "THW: Zugtrupp": filterOptions.thwZugtrupps,
+        "THW: Fachgruppe SB": filterOptions.thwSB,
+        "Drohnen-Erweiterung": filterOptions.drohnenErweiterungen,
+        "Betreuungs- und Verpflegungsdienst": filterOptions.betreuungsVerpflegung,
+        "Bereitschaftspolizeiwache": filterOptions.bereitschaftspolizei,
+        "Erweiterung für Sonderfahrzeug: Gefangenenkraftwagen": filterOptions.gefangenenkraftwagen,
+        "Züge der 1. Hundertschaft": filterOptions.hundertschaft,
+        "Verpflegungsdienst-Erweiterung": filterOptions.verpflegungsdienst,
+        "Lautsprecherkraftwagen-Erweiterung": filterOptions.lautsprecherkraftwagen,
+        "Polizei-Motorradstaffel": filterOptions.motorradstaffel,
+        "Polizeihubschrauberstation": filterOptions.polizeihubschrauber,
+        "Technischer Zuge: Wasserwerfer": filterOptions.wasserwerfer,
+        "MEK-Wache": filterOptions.mekWache,
+        "SEK-Wache": filterOptions.sekWache,
+        "NEA50-Erweiterung": filterOptions.nea50,
+        "NEA200-Erweiterung": filterOptions.nea200,
+        "Bergrettungswache": filterOptions.bergrettungswache,
+        "Seenotrettungswache": filterOptions.seenotrettungswache,
+        "Hubschrauberstationen (Seenotrettung)": filterOptions.seenothubschrauber,
+        "Reiterstaffel": filterOptions.reiterstaffel,
+        "Bahnrettungs-Erweiterung": filterOptions.bahnrettung,
+        "Lüfter-Erweiterung": filterOptions.lüfter,
+        "Windenrettungs-Erweiterungen": filterOptions.windenrettung
+    };
+
+    function hideMissions() {
+        document.querySelectorAll('.mission_type_index_searchable').forEach(el => {
+            let text = el.textContent || el.innerText;
+            // Erfolgreiche Einsätze ausblenden
+            if (hideSuccess && el.classList.contains('success')) {
+                el.style.display = 'none';
+                return;
+            }
+
+            // Prüfe alle Schlüsselwörter und deren Status
+            for (const [keyword, shouldHide] of Object.entries(keywords)) {
+                if (shouldHide && text.includes(keyword)) {
+                    el.style.display = 'none';
+                    return;
+                }
+            }
         });
     }
 
-    const observer = new MutationObserver(hideSuccessMissions);
+    // MutationObserver für dynamische Änderungen
+    const observer = new MutationObserver(hideMissions);
     observer.observe(document.body, { childList: true, subtree: true });
 
-    hideSuccessMissions();
+    // Direkt ausführen
+    hideMissions();
 })();
