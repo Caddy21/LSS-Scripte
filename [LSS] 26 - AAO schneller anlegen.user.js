39e1255f2a6da2a8c5c4c1f1c3aff381c35ec5f1
Caddy21 <163453041+Caddy21@users.noreply.github.com>
2025-06-11 16:48:03 +0200

    Update [LSS] 26 - AAO schneller anlegen.user.js

--- [LSS] 26 - AAO schneller anlegen.user.js
+++ [LSS] 26 - AAO schneller anlegen.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         AAO schneller anlegen
+// @name         [LSS] AAO schneller anlegen
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Fügt im Einsatzfenster ein Button zum AAO-Anlegen ein.
