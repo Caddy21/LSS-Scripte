1dad83ff5e6c72e99c35ae8323cedc86a3194be6
Chris <163453041+Caddy21@users.noreply.github.com>
2026-09-26 20:49:45 +0200

    Update and rename [LSS] 08 - Design Switcher.user.js to [LSS] 08 - Platzhalter.user.js

---
+++ [LSS] 08 - Platzhalter.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         [LSS] Platzhalter
+// @namespace    https://www.leitstellenspiel.de/
+// @version      1.0
+// @description  Hält ein Platz fest
+// @author       Caddy21
+// @match        https://www.leitstellenspiel.de/*
+// @match        https://polizei.leitstellenspiel.de/*
+// @icon         https://github.com/Caddy21/-docs-assets-css/raw/main/yoshi_icon__by_josecapes_dgqbro3-fullview.png
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    Hier gibt es nichts zu sehen
+
+})();
+
